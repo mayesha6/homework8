2d7047bf98222ffb7389e5bb06fca127903014fb
mayesha <soumyrhmn234@gmail.com>
2024-08-19 22:38:28 +0600

    done hw

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// function scrolltoTop(){
+//     document.getElementById("scrolltoTop").addEventListener("click", function(){
+//         window.scrollTo({ top: 0, behavior: 'smooth' });
+//     })
+// }
+const scrolltoTop = () => {
+    document.getElementById("scrolltoTop").addEventListener("click", function(){
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    })
+}
