9d7126a45d5bf3547b7476acf31df62f39ee9ada
mayesha <soumyrhmn234@gmail.com>
2024-08-20 02:58:31 +0600

    update js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,3 @@
-// function scrolltoTop(){
-//     document.getElementById("scrolltoTop").addEventListener("click", function(){
-//         window.scrollTo({ top: 0, behavior: 'smooth' });
-//     })
-// }
 const scrolltoTop = () => {
-    document.getElementById("scrolltoTop").addEventListener("click", function(){
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-    })
+    window.scrollTo({ top: 0, behavior: 'smooth' });
 }
